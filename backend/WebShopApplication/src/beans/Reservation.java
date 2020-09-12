211ab36e3ba7a8c567d9373ca02aca03160ae544
antonic901 <antonicnikola6@gmail.com>
2020-09-12 13:46:18 +0200

    sitne izmene

--- backend/WebShopApplication/src/beans/Reservation.java
+++ backend/WebShopApplication/src/beans/Reservation.java
@@ -8,5 +8,29 @@ public class Reservation {
 	private Date CheckIn; //od kad pocinje 
 	private int durationPerNight = 1 ;
 	private double price;
+	public Apartment getApartment() {
+		return apartment;
+	}
+	public void setApartment(Apartment apartment) {
+		this.apartment = apartment;
+	}
+	public Date getCheckIn() {
+		return CheckIn;
+	}
+	public void setCheckIn(Date checkIn) {
+		CheckIn = checkIn;
+	}
+	public int getDurationPerNight() {
+		return durationPerNight;
+	}
+	public void setDurationPerNight(int durationPerNight) {
+		this.durationPerNight = durationPerNight;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 	
 }
