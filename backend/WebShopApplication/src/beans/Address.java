5af42cd710af125048f6ef723882bc4ad6aad4d1
@cyberpoetry <sweetbee0071@gmail.com>
2021-01-06 13:12:18 +0100

    Adding model Address

---
+++ backend/WebShopApplication/src/beans/Address.java
@@ -0,0 +1,24 @@
+package beans;
+
+public class Address {
+	@Override
+	public String toString() {
+		return  address;
+	}
+
+	private String address="";
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Address(){}
+	
+	public Address(String street,int num,String placeName,int postalCode){
+		address=street+" "+num+","+placeName+postalCode;
+	}
+}
