80aa37da45785449ad9b4fe877ffc71db9a2bb81
@cyberpoetry <sweetbee0071@gmail.com>
2021-08-19 15:10:23 +0200

    order controller and json file

---
+++ web2021/src/main/java/web2021/controller/OrderController.java
@@ -0,0 +1,21 @@
+package web2021.controller;
+import static web2021.Application.gson;
+import static web2021.Application.orderService;
+
+
+import java.util.ArrayList;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import web2021.model.Test;
+
+public class OrderController {
+//	public static Route getAll = (Request request, Response response) -> {
+//		ArrayList<OrderService> tests = orderService.getAll();
+//		String returnToFront = gson.toJson(tests);
+//		return returnToFront;
+//	};
+//	
+  
+}
